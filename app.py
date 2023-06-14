14abcdd662239684d4849a9d38de5944787bfd83
Max <linme00p@gmail.com>
2023-06-14 18:51:50 -0400

    Base template for posting op_return.

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from bit import Key
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/', methods=['GET', 'POST'])
+def hello_world():
+    if request.method == 'POST':
+        # If submit button is pressed get the values from the form: messsage and private key.
+        message = request.form['message']
+        private_key = request.form['private']
+        # Create a key object from the private key.
+        key = Key(private_key)
+        # Send a transaction to the bitcoin network with the message as the data. Using op_return.
+        key.send("123123123", message=message)
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
