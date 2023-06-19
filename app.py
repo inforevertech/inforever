8fbb1220d7f27046ba419490660368e605e39466
lawaldemur <vnk3247@nyu.edu>
2023-06-19 19:57:02 -0400

    add BTC TestNet OP_RETURN transaction

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
 from flask import Flask, render_template, request
-from bit import Key
+from bit import PrivateKeyTestnet
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
-def hello_world():
+def index():
     if request.method == 'POST':
         # If submit button is pressed get the values from the form: messsage and private key.
         message = request.form['message']
         private_key = request.form['private']
         # Create a key object from the private key.
-        key = Key(private_key)
+        key = PrivateKeyTestnet(private_key)
         # Send a transaction to the bitcoin network with the message as the data. Using op_return.
-        key.send("123123123", message=message)
+        print("Transaction id:", key.send([('mqVyyV3bKQpMT59epHwY7zsMiKtweSyDiw', 0.00000001, 'btc')], message=message))
+
     return render_template('index.html')
 
 
 if __name__ == '__main__':
     app.run()
+
