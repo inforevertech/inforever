55f829eacafc2a50fa6a8dd769b406dfe44b2a46
lawaldemur <vnk3247@nyu.edu>
2023-06-19 20:51:17 -0400

    add mainnet/testnet switch

--- app.py
+++ app.py
@@ -11,14 +11,24 @@ def index():
         message = request.form['message']
         private_key = request.form['private']
         # Create a key object from the private key.
-        key = PrivateKeyTestnet(private_key)
-        # Send a transaction to the bitcoin network with the message as the data. Using op_return.
-        transaction_id = key.send([('mqVyyV3bKQpMT59epHwY7zsMiKtweSyDiw', 0.00000001, 'btc')], message=message)
-        transaction_url = "https://live.blockcypher.com/btc-testnet/tx/" + transaction_id + "/"
+        if request.args.get('testnet'):
+            key = PrivateKeyTestnet(private_key)
+            # Send a transaction to the bitcoin network with the message as the data. Using op_return.
+            transaction_id = key.send([('mqVyyV3bKQpMT59epHwY7zsMiKtweSyDiw', 0.00000001, 'btc')], message=message)
+            transaction_url = "https://live.blockcypher.com/btc-testnet/tx/" + transaction_id + "/"
+        else:
+            key = Key(private_key)
+            transaction_id = key.send([('148ZXqrF4FPLNyZhyUgYSFDHb7Kp9pL4TW', 0.00000001, 'btc')], message=message)
+            transaction_url = "https://live.blockcypher.com/btc/tx/" + transaction_id + "/"
 
         return render_template('success.html', transactionId=transaction_id, transactionUrl=transaction_url)
 
-    return render_template('index.html')
+    # on testnet
+    if request.args.get('testnet'):
+        return render_template('index.html', testnet=True)
+
+    # on mainnet
+    return render_template('index.html', testnet=False)
 
 
 if __name__ == '__main__':
