0eccbb20ab15e7f52652d88af0ed2c49f565ebb3
lawaldemur <vnk3247@nyu.edu>
2023-07-03 22:11:41 -0400

    add transaction exception handler and explorer response code info

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from bit import Key, PrivateKeyTestnet
+from bit import exceptions as bitExceptions
 import requests
 
 
@@ -16,12 +17,23 @@ def index():
 
         # Create a key object from the private key.
         key = PrivateKeyTestnet(private_key)
-        # Send a transaction to the bitcoin network with the message as the data. Using op_return.
-        transaction_id = key.send([], message=message)
-        transaction_url = "https://live.blockcypher.com/btc-testnet/tx/" + transaction_id + "/"
-        transaction_data = requests.get("https://blockstream.info/testnet/api/tx/" + transaction_id)
 
-        return render_template('success.html', transactionId=transaction_id, transactionUrl=transaction_url, transactionData=transaction_data)
+        try:
+            # Send a transaction to the bitcoin network with the message as the data. Using op_return.
+            transaction_id = key.send([], message=message)
+        except bitExceptions.InsufficientFunds as e:
+            # form insufficient funds description
+            errorContent = ["InsufficIent Funds.",
+                            "Error message: " + str(e),
+                            "Use the following faucet to get testnet satoshi: https://testnet-faucet.com/btc-testnet/"]
+            
+            # return page with insufficient funds information
+            return render_template('error.html', errorContent=errorContent)
+        except:
+            # return page with error description
+            return render_template('error.html', errorContent=["Something went wrong.", "Error message: " + str(e)])
+
+        return redirect("/tr/" + transaction_id)
 
     return render_template('index.html')
 
@@ -30,13 +42,23 @@ def index():
 @app.route('/tr/<transaction_id>', methods=['GET', 'POST'])
 def transaction(transaction_id):
     transaction_url = "https://live.blockcypher.com/btc-testnet/tx/" + transaction_id + "/"
-    transaction_data = requests.get("https://blockstream.info/testnet/api/tx/" + transaction_id).json()
-    
-    outputs = []
-    for line in transaction_data.get('vout'):
-        if line['scriptpubkey_type'] == 'op_return':
-            outputs.append(line['scriptpubkey_asm'])
-    transaction_data = '\n'.join(outputs)
+    transaction_data = requests.get("https://blockstream.info/testnet/api/tx/" + transaction_id)
+
+    if transaction_data.status_code == 200:
+        # form success response with decryped OP_RETURN data
+        transaction_data = transaction_data.json()
+        outputs = []
+        for line in transaction_data.get('vout'):
+            if line['scriptpubkey_type'] == 'op_return':  # only op_return statements
+                outputs.append(line['scriptpubkey_asm'])
+        transaction_data = '\n'.join(outputs)
+
+    elif transaction_data.status_code == 404:
+        # form transaction not ready or not existent response
+        transaction_data = "transaction is still waiting for acceptance in mempool or not existent."
+    else:
+        # form generic status code response
+        transaction_data = "transaction explorer response code: " + str(transaction_data.status_code)
 
     # TODO: learn do decipher scriptpubkey to extract text data
     # https://blockstream.info/testnet/api/tx/449f06d324daccfd65711e9856491f17945892491103b497baf4828b388e5c0c
